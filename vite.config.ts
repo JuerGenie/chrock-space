ffac1571c8ce92abee7c75bcac755447158591bb
Juer Genie Whang <juergenie@qq.com>
2021-07-07 21:53:40 +0800

    use base path: '/chrock-space/'

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { svelte } from "@sveltejs/vite-plugin-svelte";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
+  base: process.env.NODE_ENV === "development" ? "/" : "/chrock-space/",
   build: {
     target: ["esnext"],
     outDir: "./docs",
